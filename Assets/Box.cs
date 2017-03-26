bb46f4eb140093df5df2cc7e3eb6b12e1c30bb22
Caner TOPÇU <caner.topcu@hotmail.com>
2017-03-26 04:53:58 +0300

    DONE: -------------------------------- Create boxes with random colors, Set boxes to grids Grouping boxes Crushing grouped boxes
    TODO: 
    create just empty boxes 
    generate random 1,2,3,4 number of boxes
    put genrated boxes to grid when click the empty boxes 
    if empty box number in a group is not enough,  cant put on the group

---
+++ Assets/Box.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VertigoGame
+{
+    public class Box
+    {
+        public enum Type
+        {
+            Empty,
+            Red,
+            Green,
+            Blue,
+            Purple
+        }
+
+        /*
+       Variables
+       */
+        private Type boxType;
+        private Group group;
+        private Color boxColor;
+        private GameObject boxObject;
+        private Transform parent;
+        /*
+       Constructor
+       */ 
+        public Box(Transform parentTransform, Material boxMaterial)
+        {
+            SetBoxType((Type)(Random.Range(0,4)));
+            SetGroup(null);
+            CreateBox(parentTransform);
+            SetBoxMaterial(boxMaterial);
+        }
+
+        public Box(Type boxType,Transform parentTransform,Material boxMaterial)
+        { 
+            SetBoxType(boxType);
+            SetGroup(null);
+            CreateBox(parentTransform);
+            SetBoxMaterial(boxMaterial);
+        }
+
+       
+        /*
+        Functions
+        */
+
+        //Box Type Operations , these will use for box grouping.
+        public Type GetBoxType()
+        {
+            return boxType;
+        }
+        public void SetBoxType(Type type = Type.Empty)
+        {
+            switch(type)
+            {
+                case Type.Empty:
+                    boxColor = new Color(0.7f, 0.7f, 0.7f, 1);
+                    break;
+                case Type.Red:
+                    boxColor = new Color(1f, 0, 0, 1);
+                    break;
+                case Type.Green:
+                    boxColor = new Color(0, 1, 0, 1);
+                    break;
+                case Type.Blue:
+                    boxColor = new Color(0, 0, 1, 1);
+                    break;
+                case Type.Purple:
+                    boxColor = new Color(0.8f, 0, 0.8f, 1);
+                    break;
+            }
+
+            if(boxObject != null)
+            { 
+                boxObject.GetComponent<Renderer>().material.color = boxColor;
+            }
+
+            this.boxType = type;
+        }
+
+        //Grouping parameters
+        public Group GetGroup()
+        {
+            return group;
+        }
+        public void SetGroup(Group group) // if box doesnt have group it will be "-1"
+        {
+            this.group = group;
+        }
+
+        //Physical features
+        public void CreateBox(Transform parentTransform=null)
+        {
+            boxObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            boxObject.transform.position = new Vector3(0, 0, 0);
+            if(parentTransform!=null)
+                boxObject.transform.parent = parentTransform;
+        }
+
+        public void SetBoxMaterial(Material boxMaterial)
+        {
+            if(boxObject != null)
+            {
+                boxObject.GetComponent<Renderer>().material = boxMaterial;
+                boxObject.GetComponent<Renderer>().material.color = boxColor;
+            }
+        }
+
+        public void SetBoxPosition(Vector3 position)
+        {
+            boxObject.transform.position = position; 
+        }
+
+        ~Box()
+        { 
+        }
+    }
+
+    public class Grid
+    {
+        List<Box> gridBoxes;
+        Transform parentTransform;
+        public Grid(Transform parentTransform,List<Box> gridBoxes)
+        {
+            this.gridBoxes = gridBoxes;
+            this.parentTransform = parentTransform;
+        }
+
+        public void Execute()
+        {
+            int i = 0;
+            foreach(Box box in gridBoxes)
+            {
+                int x = i % 5;
+                int y =(int) Mathf.Floor(i / 5); 
+                Vector3 myPos = new Vector3(x, y, 0);
+                box.SetBoxPosition(myPos);
+                i++;
+            }
+            parentTransform.position = new Vector3(parentTransform.position.x-2.5f, parentTransform.position.y - 2.5f, 0);
+        }
+
+        ~Grid()
+        { 
+        }
+    }
+
+    public class Group
+    {
+        private Box.Type groupBoxType;
+        private List<Box> boxList;
+
+        //Constructors
+        public Group()
+        {
+            this.groupBoxType = Box.Type.Empty;
+            boxList = new List<Box>();
+        } 
+        public Group(Box.Type groupBoxType)
+        {
+            this.groupBoxType = groupBoxType;
+            boxList = new List<Box>();
+        }
+        //Constructors End
+
+        public Box.Type GetBoxType()
+        {
+            return groupBoxType;
+        }
+        public void AddNewBox(Box item)
+        {
+            boxList.Add(item);
+        }
+
+        public void AddNewBoxList(List<Box> items)
+        {
+            boxList.AddRange(items);
+        }
+
+        public List<Box> GetBoxList()
+        {
+            return boxList;
+        }
+
+        public void ClearGroup()
+        {
+            boxList.Clear();
+        }
+
+        public int Lenght()
+        {
+            return boxList.Count;
+        }
+
+        public void MergeWith(ref Group otherGroup)
+        {
+            AddNewBoxList(otherGroup.GetBoxList());
+            otherGroup.ClearGroup();
+        }
+
+        ~Group()
+        { 
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace VertigoGame
+{
+    public class GameManager:MonoBehaviour
+    {
+        public Transform parentTransform;
+        public Material boxMaterial;
+        List<Box> boxList;
+        Grid grid;
+        List<Group> groups;
+        public float score = 0;
+        // Use this for initialization
+        void Start()
+          {  
+             groups = new List<Group>();
+             boxList = new List<Box>();
+             for(int i = 0; i < 25; i++)
+             {
+                 Box myBox = new Box(parentTransform, boxMaterial);
+                 boxList.Add(myBox); 
+             }
+             grid = new Grid(parentTransform,boxList);
+             grid.Execute();
+             GroupBoxes();
+            
+             //int myCount = 0; 
+             //foreach(Group g in groups)
+             //{
+             //    string value="";
+             //    foreach(Box b in g.GetBoxList())
+             //    {
+             //        value +=  b.GetBoxType()+" "; 
+             //    }
+             //    print(myCount+ " "+ value);
+             //    myCount++;
+             //} 
+        } 
+
+        /// <summary>
+        /// This function organize and calculate all groups, 
+        /// these groups will used for 
+        /// </summary>
+        public void GroupBoxes()
+        {
+            for(int y = 0; y < 5; y++)// bottom to top scan 
+            {
+                for(int x = 0; x < 5; x++) // left to right scan 
+                 { 
+                    Box.Type chosen = boxList[y * 5 + x].GetBoxType(); 
+                   
+                    Group group= boxList[y * 5 + x].GetGroup();
+                    
+                    if(group == null )//Chosen box doesnt have any group 
+                    { 
+                        // Group is null , Check Right and setGroup or create new group if neighbour has a group with same box Type
+                        if(x != 4)// this is used for to get neighbour box, when we have 5 box, chosen should be max for to get 5th element as neigh. 
+                        {
+                            if(boxList[y * 5 + x + 1].GetGroup() != null) //rightNeighbour is full
+                            {
+                                Box.Type neighborRight = boxList[y * 5 + x + 1].GetBoxType();
+                                if(chosen == neighborRight) // neigh's box type is same with chosen, than get neigh's group to chosen
+                                {
+                                    group = boxList[y * 5 + x + 1].GetGroup();
+                                    boxList[y * 5 + x].SetGroup(group); // set group to box
+                                    group.AddNewBox(boxList[y * 5 + x ]); // add the box to group
+                                }
+                                else // neigh's box type is diffreent than chosen so create a new group
+                                {
+                                    group = new Group(chosen);
+                                    groups.Add(group);// add the created group to list<Group>
+                                    boxList[y * 5 + x].SetGroup(group);
+                                    group.AddNewBox(boxList[y * 5 + x]);
+                                }
+                            }
+                            else // right neighbour is empty , create new group and if neigh's type is same, set its group here like chosen
+                            {
+                                group = new Group(chosen); 
+                                groups.Add(group); // add the created group to list<Group>
+                                boxList[y * 5 + x].SetGroup(group);
+                                group.AddNewBox(boxList[y * 5 + x]); 
+
+                                Box.Type neighborRight = boxList[y * 5 + x + 1].GetBoxType();
+                                if(chosen == neighborRight)
+                                {
+                                    boxList[y * 5 + x + 1].SetGroup(group);
+                                    group.AddNewBox(boxList[y * 5 + x + 1]);
+                                }
+                            }
+                        }
+                        else // if chosen is 5th element, create new group
+                        {
+                            group = new Group(chosen);
+                            groups.Add(group); // add the created group to list<Group>
+                            boxList[y * 5 + x].SetGroup(group);
+                            group.AddNewBox(boxList[y * 5 + x]);
+                        }
+
+
+                        if( y != 4) //Set top neighbour's group like the chosen's group if it has same box type with chosen 
+                        {
+                            Box.Type neighborTop = boxList[(y + 1) * 5 + x].GetBoxType();
+                            if(chosen == neighborTop)
+                            {
+                                boxList[(y + 1) * 5 + x].SetGroup(group);
+                                group.AddNewBox(boxList[(y + 1) * 5 + x]); 
+                            }
+                        }
+                          
+                    }
+                    else //Chosen box has a group, because we might have set the group in previous process and we can face these boxes.
+                    { 
+                        if(x != 4)
+                        {
+                            Group RightGroup = boxList[y * 5 + x + 1].GetGroup();
+
+                            Box.Type neighborRight = boxList[y * 5 + x + 1].GetBoxType();
+                            if(RightGroup == null)  // if right neigh's group is null  and its box type same with chosen set its group like chosen's
+                            {  
+                                if(chosen == neighborRight) 
+                                {
+                                    boxList[y * 5 + x + 1].SetGroup(group);
+                                    group.AddNewBox(boxList[y * 5 + x + 1]);
+                                }
+                            }
+                            else if(RightGroup != null) // if right neigh has a group,
+                            {
+                                if(RightGroup != group)// and its not like chosen, 
+                                {
+                                    if(chosen == neighborRight) // but it has same type with chosen
+                                    {
+                                        foreach(Box b in RightGroup.GetBoxList())
+                                        {
+                                            b.SetGroup(group); 
+                                        }
+                                        group.MergeWith(ref RightGroup); // merge the group boxes and set it on chosen's group; 
+                                        // g1=a,b,c , g2 = d,f,g,h g1.MergeWith(g2)==> g1 = a,b,c,d,f,g,h , g2 = null 
+
+                                        groups.Remove(RightGroup); //Remove from the list
+                                        RightGroup = null;
+                                    }
+                                }
+                            }
+                        } 
+                         
+                        if(y != 4)// look at top if it's like chosen, set its group identy as chosen's group 
+                        { 
+                            Box.Type neighborTop = boxList[(y + 1) * 5 + x].GetBoxType(); 
+                            if(chosen == neighborTop)
+                            {
+                                boxList[(y + 1) * 5 + x].SetGroup(group);
+                                group.AddNewBox(boxList[(y + 1) * 5 + x]); 
+                            }
+                             
+                        }
+                    }  
+               }
+            } 
+        }
+
+        /// <summary> 
+        /// GroupCrusher crushes the boxes which are not empty and member number is more than 3  
+        /// </summary>
+        public void GroupCrusher()
+        { 
+                foreach(Group g in groups)
+                {
+                    if(g.GetBoxType() != Box.Type.Empty)
+                    {
+                        if(g.GetBoxList().Count >= 3)
+                        {
+                            score += g.GetBoxList().Count * 10;
+                            foreach(Box b in g.GetBoxList())
+                            {
+                                b.SetBoxType(Box.Type.Empty);
+                            }
+                        }
+                    }
+                }
+
+            ClearGroups();
+        }
+
+        public void ClearGroups()
+        {
+            groups.Clear();
+            foreach(Box b in boxList)
+            {
+                b.SetGroup(null);
+            }
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
